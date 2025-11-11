6ae813ae0a504877ba77fc9c9eee0a715d9fc31f
kennedfer <kenned14santos@gmail.com>
2025-11-11 12:32:27 -0300

    fix: Remove getPhone

--- src/phone/phone.repo.ts
+++ src/phone/phone.repo.ts
@@ -4,32 +4,6 @@ import { BadRequestError, InternalServerError, NotFoundError } from "../errors";
 import { Trace } from "../tracing";
 
 export class PhoneRepo {
-  async getPhone(id: string) {
-    try {
-      const phone = await db
-        .selectFrom("phones")
-        .selectAll()
-        .where("id", "=", id)
-        .executeTakeFirst();
-      if (!phone) {
-        // Só para testes
-        // throw new NotFoundError("phone");
-
-        return {
-          id: "",
-          phone_number: "5598970204218",
-          user_id: "sla",
-        };
-      }
-      return phone;
-    } catch (error: any) {
-      if (error instanceof NotFoundError) {
-        throw error;
-      }
-      throw new InternalServerError("Failed to get phone", { cause: error });
-    }
-  }
-
   @Trace({ spanName: "phoneRepo.insertPhone" })
   async insertPhone(input: NewPhone): Promise<string> {
     try {
