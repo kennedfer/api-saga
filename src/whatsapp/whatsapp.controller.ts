38ecb22636b618d833d8fa20b0dc670a3e3472de
kennedfer <kenned14santos@gmail.com>
2025-11-11 16:44:40 -0300

    fix: Add tracing to WhatsappController

--- src/whatsapp/whatsapp.controller.ts
+++ src/whatsapp/whatsapp.controller.ts
@@ -1,17 +1,21 @@
 import type { Request, Response } from "express";
 import { whatsappService } from "./whatsapp.service";
+import { Trace } from "../tracing";
 
 export class WhatsappController {
+  @Trace({spanName: "whatsappController.create"})
   async create(req: Request, res: Response) {
     const created = await whatsappService.create(req.body);
     res.json(created);
   }
 
+  @Trace({spanName: "whatsappController.connect"})
   async connect(req: Request, res: Response) {
     const response = await whatsappService.connect(req.params.phone_id);
     res.json(response);
   }
 
+  @Trace({spanName: "whatsappController.disconnect"})
   async disconnect(req: Request, res: Response) {
     const response = await whatsappService.disconnect(req.params.phone_id);
     res.json(response);
