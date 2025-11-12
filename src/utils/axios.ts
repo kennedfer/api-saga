13cd3dd4620b233e0259d57c00338ef56763058e
kennedfer <kenned14santos@gmail.com>
2025-11-12 16:41:57 -0300

    feat: Implement asaasApi wrapper to requests to asaas

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,10 +1,21 @@
 import axios from "axios";
+import { configDotenv } from "dotenv";
+configDotenv();
 
-export const api = axios.create({
+export const evolutionApi = axios.create({
   baseURL: process.env.EVOLUTION_API_PATH,
   timeout: 15000,
   headers: {
     "Content-Type": "application/json",
     apikey: process.env.EVOLUTION_API_KEY,
+  },
+});
+
+export const asaasApi = axios.create({
+  baseURL: process.env.ASAAS_API_PATH,
+  timeout: 15000,
+  headers: {
+    Accept: "application/json",
+    access_token: process.env.ASAAS_API_KEY,
   },
 });
