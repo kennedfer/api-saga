fda82f86a3cbc15216d37e5c6532ee0a67d8c3ce
kennedfer <kenned14santos@gmail.com>
2025-11-13 16:57:41 -0300

    fix: Remove payload example of service

--- src/asaas-webhook/asaas-webhook.service.ts
+++ src/asaas-webhook/asaas-webhook.service.ts
@@ -4,32 +4,6 @@ import { mapWebhookToDb } from "../utils/mapper";
 import { userPlanService } from "../user-plan/user-plan.service";
 import { paymentsHistoryRepo } from "../payments-history/payments-history.repo";
 
-// {
-//   id: 'evt_20f793f686aa4783d486a40e3c6b91d1&12558163',
-//   event: 'CHECKOUT_PAID',
-//   dateCreated: '2025-11-12 21:14:57',
-//   checkout: {
-//     id: '15e844d1-b9ed-48fe-a202-b28b1e56057a',
-//     link: null,
-//     status: 'PAID',
-//     minutesToExpire: 1440,
-//     externalReference: 'Plano Standard - 1610b448-26c4-445d-86c8-e700f477b6bd',
-//     billingTypes: [ 'PIX' ],
-//     chargeTypes: [ 'DETACHED' ],
-//     callback: {
-//       cancelUrl: 'https://eighty-banks-sleep.loca.lt/cancel',
-//       successUrl: 'https://eighty-banks-sleep.loca.lt/success',
-//       expiredUrl: 'https://eighty-banks-sleep.loca.lt/expired'
-//     },
-//     items: [ [Object] ],
-//     subscription: null,
-//     installment: null,
-//     split: null,
-//     customer: null,
-//     customerData: null
-//   }
-// }
-
 export class AsaasWebhookService {
   @Trace({ spanName: "asaasWebhookService.processPayment" })
   async processPayment(payment: any): Promise<void> {
