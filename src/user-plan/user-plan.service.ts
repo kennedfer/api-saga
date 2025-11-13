0118a1c7198ab56ee0384342638379c0e3877374
kennedfer <kenned14santos@gmail.com>
2025-11-13 16:58:03 -0300

    fix: Better time calculation

--- src/user-plan/user-plan.service.ts
+++ src/user-plan/user-plan.service.ts
@@ -1,12 +1,15 @@
+import { Trace } from "../tracing";
 import { userPlanRepo } from "./user-plan.repo";
 
 const DAYS_TO_ADD = 30;
+const ONE_HOUR_IN_MILIS = 3_600_000;
 
 class UserPlanService {
+  @Trace({spanName: "userPlanService.addPlanToUser"})
   async addPlanToUser(userId: string, planId: string) {
     const startDate = new Date();
     const expirationDate = new Date(
-      Date.now() + 1000 * 60 * 60 * 24 * DAYS_TO_ADD
+      Date.now() + ONE_HOUR_IN_MILIS * 24 * DAYS_TO_ADD
     );
 
     const userPlanId = userPlanRepo.insertPlan({
